f406eecc27b7ebb8d564c675bdd4c3d2854b2189
marc <marcsmithr@gmail.com>
2023-06-01 16:23:30 -0500

    added google maps api

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,11 @@ import { useLoadScript } from "@react-google-maps/api";
 import Map from "../components/map";
 
 export default function Home() {
-  const { isLoaded } = useLoadScript({
+  const {isLoaded} = useLoadScript({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-    libraries: ["places"],
+    libraries: ["places"]
   });
 
-  if (!isLoaded) return <div>Loading...</div>;
+  if (!isLoaded) return <div> Loading ...</div>
   return <Map />;
 }
